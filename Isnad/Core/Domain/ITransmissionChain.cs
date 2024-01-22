1d2c8bf0e2a54364b27f465688e53ec9cd1d45f3
vsvincent <vincentvs01@yahoo.com>
2024-01-22 21:39:21 +0100

    Add ITransmissionChain

---
+++ Isnad/Core/Domain/ITransmissionChain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    /// <summary>
+    /// Represent an entire chain of transmission of a hadith
+    /// </summary>
+    public interface ITransmissionChain
+    {
+        /// <summary>
+        /// A list of transmissions in the chain with index indicating the order of transmission
+        /// </summary>
+        IList<ITransmission> Transmissions { get; set; }
+    }
+}

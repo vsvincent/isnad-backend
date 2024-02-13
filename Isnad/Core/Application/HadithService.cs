20f0f6763fc6a9aa420b0b32195f1b1721e4983d
vsvincent <vincentvs01@yahoo.com>
2024-02-13 21:03:35 +0100

    Use hadith repository

--- Isnad/Core/Application/HadithService.cs
+++ Isnad/Core/Application/HadithService.cs
@@ -1,7 +1,33 @@
-﻿namespace Application
+﻿using Domain;
+using Repository;
+
+namespace Application
 {
     public class HadithService : IHadithService
     {
+        IHadithRepository _hadithRepository;
+        public HadithService(IHadithRepository hadithRepository)
+        {
+            _hadithRepository = hadithRepository;
+        }
+        public void AddHadith(IHadith hadith)
+        {
+            _hadithRepository.AddHadith(hadith);
+        }
 
+        public void DeleteHadithById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IHadith> GetHadithById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHadithById(Guid id, IHadith hadith)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

2574fe0fdd401dab9eb4d3358298bb86877a71ac
vsvincent <vincentvs01@yahoo.com>
2024-02-13 22:12:14 +0100

    Add repository to services

--- Isnad/WebAPI/Program.cs
+++ Isnad/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHadithService, HadithService>();
+builder.Services.AddScoped<IHadithRepository, HadithRepository>();
 
 var app = builder.Build();
 

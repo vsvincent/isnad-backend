cf6c6b6e41c505d21f436fe0383483c1371f6adc
vsvincent <vincentvs01@yahoo.com>
2024-02-13 21:03:22 +0100

    Add hadith repository class

---
+++ Isnad/Repository/HadithRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain;
+using Neo4j.Driver;
+
+namespace Repository
+{
+    public class HadithRepository : IHadithRepository
+    {
+        private readonly Neo4jDatabaseManager _dbManager = new Neo4jDatabaseManager("", "", "");
+
+        public HadithRepository()
+        {
+        }
+
+        public async Task AddHadith(IHadith hadith)
+        {
+            using (IAsyncSession session = _dbManager.GetSession())
+            {
+                await session.ExecuteWriteAsync(
+            async tx =>
+            {
+                var result = await tx.RunAsync(
+                    "CREATE (h:Hadith {title: $title, text: $text, matn: $matn, sanad: $sanad})",
+                    new { hadith.Title, hadith.Text, hadith.Matn, hadith.Sanad });
+            });
+            }
+        }
+
+        public void DeleteHadithById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IHadith> GetHadithById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHadithById(Guid id, IHadith hadith)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

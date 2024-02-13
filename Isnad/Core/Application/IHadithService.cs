944bda0b5d6a9042d36b039836914b9ddc90f094
vsvincent <vincentvs01@yahoo.com>
2024-02-13 19:52:07 +0100

    Update IHadithService.cs

--- Isnad/Core/Application/IHadithService.cs
+++ Isnad/Core/Application/IHadithService.cs
@@ -5,8 +5,8 @@ namespace Application
     public interface IHadithService
     {
         Task<IHadith> GetHadithById(Guid id);
-        void AddHadithById(Guid id);
-        void UpdateHadithById(Guid id);
+        void AddHadith(IHadith hadith);
+        void UpdateHadithById(Guid id, IHadith hadith);
         void DeleteHadithById(Guid id);
     }
 }
